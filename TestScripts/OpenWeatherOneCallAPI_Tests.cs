a4599ebd22a03d26bd61cc23dc60c2fa7f219924
Rebekah <rebekah.pool90@gmail.com>
2022-10-30 22:11:14 +0000

    Simplified tests

--- TestScripts/OpenWeatherOneCallAPI_Tests.cs
+++ TestScripts/OpenWeatherOneCallAPI_Tests.cs
@@ -24,31 +24,15 @@ namespace WeatherAPI_Test_Project.TestScripts
             };
         }
 
-        //Cannot progress test without subscribing
         [DataTestMethod]
         [DataRow("London", "1")]
-        public void Coordinates_Return_SuccessResponse_From_OpenWeatherOneCallAPI(string cityName, string limit)
+        public void Valid_Coordinates_Return_SuccessResponse_From_OpenWeatherOneCallAPI(string cityName, string limit)
         {
             var geoCoding = GetCoordinatesFromGeocodingAPI(cityName, limit);
 
             var response = client.GetAsync($"data/3.0/onecall?lat={geoCoding[0].lat}&lon={geoCoding[0].lat}&appid={APIKey}").Result;
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, "StatusCode is not as expected");
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, $"StatusCode is incorrect. Error Message {response.Content.ReadAsStringAsync().Result.ToString()}");
         }
-
-        [TestMethod]
-        public void IfUserDoesNotHaveSubscriptionThenOpenWeatherOneCallAPIReturnsUnauthorisedResponse()
-        {
-            var geoCoding = GetCoordinatesFromGeocodingAPI();
-
-            var response = client.GetAsync($"data/3.0/onecall?lat={geoCoding[0].lat}&lon={geoCoding[0].lon}&appid={APIKey}").Result; 
-            Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode, "StatusCode is not as expected");
-            var deserialisedResponse = JsonConvert.DeserializeObject<OpenWeatherAPI401Response.Rootobject>(response.Content.ReadAsStringAsync().Result);
-            Assert.AreEqual(401, deserialisedResponse.cod, "Unauthorised respose Cod value is not as expected");
-            Assert.AreEqual("Please note that using One Call 3.0 requires a separate subscription to the One Call by Call plan. Learn more here https://openweathermap.org/price. If you have a valid subscription to the One Call by Call plan, but still receive this error, then please see https://openweathermap.org/faq#error401 for more info.",
-                    deserialisedResponse.message, "Unautorised message text is not as expected");
-        }
-
-
 
         public GeocodingAPI.Rootobject[] GetCoordinatesFromGeocodingAPI(string cityName = "London", string limit = "1")
         {
