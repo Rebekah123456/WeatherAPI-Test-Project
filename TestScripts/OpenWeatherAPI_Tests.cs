3bff35fe7e0a593d56fb5f633a0baf23e39d03dd
Rebekah <rebekah.pool90@gmail.com>
2022-10-31 09:31:45 +0000

    Added additional Current Weather API call

--- TestScripts/OpenWeatherAPI_Tests.cs
+++ TestScripts/OpenWeatherAPI_Tests.cs
@@ -10,7 +10,7 @@ using System.Net;
 namespace WeatherAPI_Test_Project.TestScripts
 {
     [TestClass]
-    public class OpenWeatherOneCallAPI_Tests
+    public class OpenWeatherAPI_Tests
     {
         private HttpClient client;
         private string APIKey = "24309239b91ac6be65a414546a4720a2";
@@ -36,6 +36,19 @@ namespace WeatherAPI_Test_Project.TestScripts
 
         [DataTestMethod]
         [DataRow("London", "1")]
+        [DataRow("Paris", "1")]
+        public void Valid_Coordinates_Return_SuccessResponse_From_CurrentWeatherAPI(string cityName, string limit)
+        {
+            var geoCodingResponse = GeocodingAPIResponse(cityName, limit);
+            var geocodingResponseData = DeserialiseGeocodingAPIResponse(geoCodingResponse);
+            var response = CurrentWeatherAPIResponse(geocodingResponseData[0].lat.ToString(), geocodingResponseData[0].lon.ToString());
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, $"StatusCode is incorrect. Error Message {response.Content.ReadAsStringAsync().Result.ToString()}");
+            var responseData = DeserialiseCurrentWeatherAPIResponse(response);
+            Assert.AreEqual(cityName, responseData.name, "Place name returned does nt match place name requested");
+        }
+
+        [DataTestMethod]
+        [DataRow("London", "1")]
         //[DataRow("Lndon", "1")]
         public void Valid_CityName_Returns_SuccessResponse_From_GeocodingAPI(string cityName, string limit)
         {
@@ -51,9 +64,19 @@ namespace WeatherAPI_Test_Project.TestScripts
             return JsonConvert.DeserializeObject<GeocodingAPI.Rootobject[]>(geocodingAPIResponse.Content.ReadAsStringAsync().Result);
         }
 
+        public CurrentWeatherAPI.Rootobject DeserialiseCurrentWeatherAPIResponse(HttpResponseMessage currentWeatherAPIResponse)
+        {
+            return JsonConvert.DeserializeObject<CurrentWeatherAPI.Rootobject>(currentWeatherAPIResponse.Content.ReadAsStringAsync().Result);
+        }
+
         public HttpResponseMessage OpenWeatherOneCallAPIResponse(string lat, string lon)
         {
             return client.GetAsync($"data/3.0/onecall?lat={lat}&lon={lon}&appid={APIKey}").Result;
+        }
+
+        public HttpResponseMessage CurrentWeatherAPIResponse(string lat, string lon)
+        {
+            return client.GetAsync($"data/2.5/weather?lat={lat}&lon={lon}&appid={APIKey}").Result;
         }
 
         public HttpResponseMessage GeocodingAPIResponse(string cityName = "London", string limit = "1")
